bbf9fb59a478c1f23fd71e766848c68b10136a6a
Shashidhar G A <shashidhar.a@hebbalelabs.com>
2019-02-21 16:52:52 +0530

    Update RunLengthEncoder.java

--- src/main/java/com/talfinder/assessment/RunLengthEncoder.java
+++ src/main/java/com/talfinder/assessment/RunLengthEncoder.java
@@ -7,7 +7,7 @@ public class RunLengthEncoder {
      * NOTE:
      * 1. Write your solution within this method
      * 2. Do not make changes to the Class Name, Method Name or Method Signature
-     * 3. Run the command Run-Java to run main method
+     * 3. Use the command Run-Java to run main method
      * 4. Use the BuildAndRunTests command to evaluate your solution
      */
     String output = null;
