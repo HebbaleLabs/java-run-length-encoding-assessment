f0ee6b944134aef34452a2c12dbd9984ac123ef4
hbhargav <harsha.bhargav@hebbalelabs.com>
2019-03-21 11:46:18 +0530

    Updated Test Case to be Parametrized

--- src/test/java/com/talfinder/assessment/RunLengthEncoderTest.java
+++ src/test/java/com/talfinder/assessment/RunLengthEncoderTest.java
@@ -1,67 +1,38 @@
 package com.talfinder.assessment;
 
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.*;
-
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class RunLengthEncoderTest {
 
-  @Test
-  public void encodeEmptyString() {
-    String encodedValue = RunLengthEncoder.encode("");
-    assertThat(encodedValue, is(equalTo("")));
+  @Parameters
+  public static Collection<Object[]> parameters() {
+    return Arrays.asList(new Object[][] {
+        {"abc", "a1b1c1"},
+        {"aaabaaa", "a3b1a3"},
+        {"mmmrrrqqqpsssssmmm", "m3r3q3p1s5m3"},
+        {"rrrssttssrrr", "r3s2t2s2r3"},
+        {"hhhaaarrssshhhaa", "h3a3r2s3h3a2"},
+        {"xyzzyxxyz", "x1y1z2y1x2y1z1"}
+    });
+  }
+
+  private String input;
+  private String expectedValue;
+
+  public RunLengthEncoderTest(String input, String expectedValue) {
+    this.input = input;
+    this.expectedValue = expectedValue;
   }
 
   @Test
-  public void encodeSingleCharacters() {
-    String encodedValue = RunLengthEncoder.encode("a");
-    assertThat(encodedValue, is(equalTo("a1")));
-
-    encodedValue = RunLengthEncoder.encode("b");
-    assertThat(encodedValue, is(equalTo("b1")));
-
-    encodedValue = RunLengthEncoder.encode("m");
-    assertThat(encodedValue, is(equalTo("m1")));
-
-    encodedValue = RunLengthEncoder.encode("y");
-    assertThat(encodedValue, is(equalTo("y1")));
-
-    encodedValue = RunLengthEncoder.encode("z");
-    assertThat(encodedValue, is(equalTo("z1")));
-  }
-
-  @Test
-  public void encodeStringWithUniqueCharacters() {
-    String encodedValue = RunLengthEncoder.encode("abc");
-    assertThat(encodedValue, is(equalTo("a1b1c1")));
-
-    encodedValue = RunLengthEncoder.encode("mnopq");
-    assertThat(encodedValue, is(equalTo("m1n1o1p1q1")));
-
-    encodedValue = RunLengthEncoder.encode("uvwxyz");
-    assertThat(encodedValue, is(equalTo("u1v1w1x1y1z1")));
-  }
-
-  @Test
-  public void encodeStringWithSequentialCharacters() {
-    String encodedValue = RunLengthEncoder.encode("aaa");
-    assertThat(encodedValue, is(equalTo("a3")));
-
-    encodedValue = RunLengthEncoder.encode("aaabbc");
-    assertThat(encodedValue, is(equalTo("a3b2c1")));
-
-    encodedValue = RunLengthEncoder.encode("mnnooo");
-    assertThat(encodedValue, is(equalTo("m1n2o3")));
-  }
-
-  @Test
-  public void encodeStringWithMultipleSequentialCharacters() {
-    String encodedValue = RunLengthEncoder.encode("aaabaaa");
-    assertThat(encodedValue, is(equalTo("a3b1a3")));
-
-    encodedValue = RunLengthEncoder.encode("rrrssttssrrr");
-    assertThat(encodedValue, is(equalTo("r3s2t2s2r3")));
+  public void encodeString() {
+    Assert.assertEquals("input:" + input , expectedValue, RunLengthEncoder.encode(input));
   }
 }
